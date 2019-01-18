a32ae74fec9b42595959e0749ac3d19dbe7a9750
Mihai Cioban <ciobanmihai@gmail.com>
2019-01-18 19:16:55 +0200

    Added CORS support

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const router = require('./router');
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/auth');
 
 const app = express();
 app.use(morgan('combined'));
+app.use(cors()); // accepts requests from anywhere without any configuration
 app.use(bodyParser.json());
 router(app);
 
